b379edc32dcdfda788e58c52d4658e79ee3ad8b1
Naeem <inaeem@protonmail.com>
2024-07-07 16:34:07 +0500

    :card_file_box: configured `redis` db

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
-export default function Home() {
+import { db } from '@/app/lib/db';
+
+export default async function Home() {
+  const counter = await db.incr('counter');
+
   return (
     <main>
-      <h1>Hello World</h1>
+      <h1>Hello World {counter}</h1>
     </main>
   );
 }
